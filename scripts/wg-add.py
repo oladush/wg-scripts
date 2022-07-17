5c779cdc5d3b81ec9df3dcf9df6d1835930a654c
admin <oladushek.hokinga@gmail.com>
2022-07-17 12:42:07 +0900

    upload wg-add

---
+++ scripts/wg-add.py
@@ -0,0 +1,97 @@
+import re
+import ipaddress
+import subprocess
+
+from typing import Optional
+
+CONFIG_PATH = "/etc/wireguard/wg0.conf"
+CLIENT_PATH = "/home/old/wireguard/clients/"
+SERVER_PUBLIC_KEY = "UUJ/uhzzf5QcSRjA5Qz/CpCFeXjUFLyx6g3U9oA3/1w="
+V_NETWORK = "10.0.0.0/24"
+
+template_user_config = """[Interface]
+PrivateKey = {pr_key}
+Address = {v_addr}
+DNS = 8.8.8.8
+
+[Peer]
+PublicKey = UUJ/uhzzf5QcSRjA5Qz/CpCFeXjUFLyx6g3U9oA3/1w=
+AllowedIPs = 0.0.0.0/0
+Endpoint = 185.18.54.215:51820
+"""
+template_server_config = """
+[Peer]
+PublicKey = {pu_key}
+AllowedIPs = {v_addr}/32
+#{description}
+"""
+
+
+def make_configs(v_ip: str, user: str) -> (str, str):
+    print("Starting key generation...")
+
+    pr_key = subprocess.check_output("wg genkey", shell=True).decode()[:-1]
+    pu_key = subprocess.check_output(f"echo {pr_key} | wg pubkey", shell=True).decode()[:-1]
+
+    print("Private key: %s" % pr_key)
+    print("Public key: %s" % pu_key)
+
+    client_config = template_user_config.format(pr_key=pr_key, v_addr=v_ip)
+    server_config = template_server_config.format(pu_key=pu_key, v_addr=v_ip, description=user)
+
+    return client_config, server_config
+
+
+def search_available_ip() -> Optional[str]:
+    with open(CONFIG_PATH, "r") as rf:
+        config = rf.read()
+
+    for ip in ipaddress.IPv4Network(V_NETWORK):
+        if not re.search(str(ip), config):
+            return str(ip)
+
+
+def write_server_config(conf):
+    with open(CONFIG_PATH, "a") as wf:
+        wf.write(conf)
+
+def write_client_config(conf, user):
+    path = CLIENT_PATH + user + '/'
+    print(f"Creating directory: { path }")
+    subprocess.check_output(f"mkdir { path }", shell=True)
+
+    with open(f"{ path + user }.conf", "w") as wf:
+        wf.write(conf)
+
+    qr = subprocess.check_output(f"qrencode -t ansiutf8 < { path + user }.conf", shell=True)
+    subprocess.check_output(f"qrencode -t png -o { path }qr.png -r { path + user }.conf", shell=True)
+    subprocess.check_output(f"qrencode -t ansiutf8 < { path + user }.conf > { path }qr.txt", shell=True)
+    print(qr.decode())
+
+
+if __name__ == '__main__':
+    print("Searching available ip..")
+    ip = search_available_ip()
+
+    if ip:
+        print("Success, ip is %s" % ip)
+    else:
+        print("Available is not founded")
+        exit(1)
+
+    username = input("Type name of new user\n# ")
+
+    if not username:
+        print("Set name of user")
+        exit(1)
+
+    print("Making configs..")
+    client, server = make_configs(ip, username)
+
+    write_server_config(server)
+    write_client_config(client, username)
+
+    print("You can type it:")
+    print("cat /etc/wireguard/wg0.conf")
+    print("wg-quick down wg0; wg-quick up wg0")
+    print(f"cd {CLIENT_PATH}")
