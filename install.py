f90e473a9e4638190503cd34778062e1e9f1398e
admin <oladushek.hokinga@gmail.com>
2022-07-17 13:59:19 +0900

    for test

--- install.py
+++ install.py
@@ -9,12 +9,6 @@ commands = [
     "cp scripts/wg-add.py /usr/local/bin/wg-add",
     "chmod u+x /usr/local/bin/wg-add",
 ]
-subprocess.check_output(f"pip install -r requirements.txt", shell=True)
-subprocess.check_output(f"mkdir /usr/local/etc/wg-scripts", shell=True)
-subprocess.check_output(f"cp configs/wg_add_config.py /usr/local/etc/wg-scripts/wg_add_config.py", shell=True)
-subprocess.check_output(f"cp scripts/wg-add.py /usr/local/bin/wg-add", shell=True)
-subprocess.check_output(f"chmod u+x /usr/local/bin/wg-add", shell=True)
-
 
 for command in commands:
     print(command)
