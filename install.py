1bcda0e40b78cb471e78190ea156a3d27e580368
admin <oladushek.hokinga@gmail.com>
2022-08-17 02:30:13 +0900

    add del script

--- install.py
+++ install.py
@@ -11,21 +11,44 @@ commands = [
     "mkdir /usr/local/wireguard",
     "mkdir /usr/local/wireguard/clients",
 
-    "cp configs/wg_add_config.py /usr/local/etc/wg-scripts/wg_add_config.py",   # for wg-add
+    # for wg-add
     "cp scripts/wg-add.py /usr/local/bin/wg-add",
     "chmod u+x /usr/local/bin/wg-add",
 
-    "cp configs/wg_show_config.py /usr/local/etc/wg-scripts/wg_show_config.py",  # for wg-show
+    # for wg-show
     "cp scripts/wg-show.py /usr/local/bin/wg-show",
     "chmod u+x /usr/local/bin/wg-show",
+
+    # for wg-del
+    "cp scripts/wg-del.py /usr/local/bin/wg-del",
+    "chmod u+x /usr/local/bin/wg-del",
 ]
 
-for command in commands:
-    print(command)
-    try:
-        res = subprocess.check_output(command, shell=True)
-    except subprocess.CalledProcessError as ex:
-        res = ex.output.decode()
+copy_configs = \
+    [
+        "cp configs/wg_add_config.py /usr/local/etc/wg-scripts/wg_add_config.py",
+        "cp configs/wg_show_config.py /usr/local/etc/wg-scripts/wg_show_config.py",
+    ]
 
-    if res:
-        print(str(res))
+
+def execute(commands: list):
+    for command in commands:
+        print(command)
+        try:
+            res = subprocess.check_output(command, shell=True)
+        except subprocess.CalledProcessError as ex:
+            res = ex.output.decode()
+
+        if res:
+            print(str(res))
+
+
+if __name__ == "__main__":
+    execute(commands)
+
+    while (answer := input('Do you want to update the config files y/n: ')) not in ('y', 'n'):
+        print('Incorrect type "y" or "n" (Ctrl+ C to exit)')
+
+    if answer == 'y':
+        execute(copy_configs)
+
---
+++ scripts/wg-del.py
@@ -0,0 +1,67 @@
+import re
+import sys
+import colorama
+
+config_path = "/usr/local/etc/wg-scripts/"
+sys.path.append(config_path)
+from wg_add_config import *
+
+
+def text_highlighting(pattern: str, text: str):
+    return re.sub(pattern, colorama.Back.RED + pattern + colorama.Back.RESET, text)
+
+
+def delete_items_from_text(items: list, text: str):
+    for item in items:
+        text = text.replace("[Peer]\n" + item, '')
+    return text
+
+
+def main():
+    with open(CONFIG_PATH, "r") as rf:
+        config = rf.read()
+
+    peers = config.split('[Peer]\n')
+    del peers[0]
+
+    colorama.init()
+
+    while 1:
+        matched = []
+
+        reg = input("Type key for search\n~# ")
+
+        for peer in peers:
+            if re.search(reg, peer):
+                matched.append(peer)
+
+        print('The result of search:')
+        for m in matched:
+            print('[Peer]\n' + text_highlighting(reg, m).strip() + '\n')
+
+        print(f'Matched peers: {len(matched)}/{len(peers)}')
+
+        while (answer := input('Do you really want to delete this items y/n: (n to research)')) not in ('y', 'n'):
+            print('Incorrect type "y" or "n" (Ctrl+ C to exit)')
+
+        if answer == 'y':
+            break
+
+    print('New config files:\n"""')
+    print(new_config := delete_items_from_text(matched, config), end='"""\n')
+
+    while (answer := input('Please confirm the changes y/n: ')) not in ('y', 'n'):
+        print('Incorrect type "y" or "n" (Ctrl+ C to exit)')
+
+    if answer == 'y':
+        with open(CONFIG_PATH, 'w') as wf:
+            wf.write(new_config)
+
+        print('Success!')
+        print("You can type it:")
+        print(f"cat {CONFIG_PATH}")
+        print("wg-quick down wg0; wg-quick up wg0")
+
+
+if __name__ == "__main__":
+    main()
